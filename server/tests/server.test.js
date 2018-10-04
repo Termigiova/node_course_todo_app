80d7f85f928c8ed7d6cbb92448c548f1a33d962f
Termigiova <termigiova@me.com>
2018-10-03 20:05:51 -0500

    Add DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -215,7 +215,7 @@ describe('POST /users', () => {
                     expect(user).toBeDefined();
                     expect(user.password).not.toBe(password);
                     done();
-                })
+                }).catch((e) => done(e));
             });
     });
 
@@ -239,5 +239,46 @@ describe('POST /users', () => {
             })
             .expect(400)
             .end(done);
+    });
+
+    describe('POST /users/login', () => {
+        it('should login user and return auth token', (done) => {
+            request(app)
+                .post('/users/login')
+                .send({
+                    email: users[1].email,
+                    password: users[1].password
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).toBeDefined();
+                })
+                .end((err, res) => {
+                    if (err)
+                        return done(err);
+
+                    User.findById(users[1]._id).then((user) => {
+                        expect(user.tokens[1]).toMatchObject({
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        });
+                        done();
+                    }).catch((e) => done(e));
+                });
+        });
+
+        it('should reject invalid login', (done) => {
+            request(app)
+                .post('/users/login')
+                .send({
+                    email: users[1].email,
+                    password: users[1].password + '1'
+                })
+                .expect(400)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).not.toBeDefined();
+                })
+                .end(done);
+        });
     });
 });
