9850046348e984c22e6cd39a0c2d2f4c1d9d5206
Termigiova <termigiova@me.com>
2018-10-04 07:05:21 -0500

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -26,12 +26,14 @@ const users = [{
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: 'Second test todo',
     completed: true,
-    completedAt: 69
+    completedAt: 69,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
